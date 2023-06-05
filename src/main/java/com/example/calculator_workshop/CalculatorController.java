c57c20e5ceeeff18f31b5b50ab735fdc9ec8488e
neda zarei <neda.z2003@gmail.com>
2023-06-05 17:43:39 +0330

    finished calculator controller, works with mouse and keyboard

--- src/main/java/com/example/calculator_workshop/CalculatorController.java
+++ src/main/java/com/example/calculator_workshop/CalculatorController.java
@@ -4,7 +4,65 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CalculatorController {
-   
+    @FXML
+    private TextField textResult;
+    private String operation = "";
+    private long firstNumber;
+    private long secondNumber;
+
+    public void number(ActionEvent ae){
+        Button button = (Button) ae.getSource();
+        String num = (button).getText();
+        textResult.setText(textResult.getText() + num);
+    }
+    public void operation(ActionEvent ae){
+        Button button = (Button) ae.getSource();
+        String op = (button).getText();
+        if(button.getText().equals("c")){
+            textResult.setText("");
+        }
+        else if(!op.equals("=")){
+            if(!operation.equals("")){
+                return;
+            }
+            operation = op;
+            firstNumber = Long.parseLong(textResult.getText());
+            textResult.setText("");
+        }
+        else{
+            if(operation.equals("")){
+                return;
+            }
+            secondNumber = Long.parseLong(textResult.getText());
+            calculate(firstNumber,secondNumber,operation);
+            operation = "";
+        }
+    }
+
+    public void calculate(long firstNumber , long secondNumber , String operation){
+        switch (operation){
+            case "*" :
+                textResult.setText(firstNumber*secondNumber+"");
+                break;
+            case "/" :
+                if(secondNumber == 0){
+                    textResult.setText("0");
+                }
+                else textResult.setText(firstNumber/secondNumber + "");
+                break;
+            case "-" :
+                textResult.setText(firstNumber-secondNumber + "");
+                break;
+            case "+" :
+                textResult.setText(firstNumber+secondNumber + "");
+                break;
+            case "%" :
+                textResult.setText(firstNumber%secondNumber + "");
+                break;
+        }
+
+    }
 }
